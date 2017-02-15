c495f0b34a30856b154d0255f0824e48d3f135b8
eaaho <esa.aho@student.saimia.fi>
2017-02-15 13:39:47 +0200

    Lukujen sorttausohjelma

--- Sorttaus/Program.cs
+++ Sorttaus/Program.cs
@@ -10,6 +10,29 @@ namespace Sorttaus
     {
         static void Main(string[] args)
         {
+            int[] Jono = new int[3];
+            int[] Result = new int[3];
+            int Dummy;
+            bool Tulos;
+            bool Pienin;
+
+            for (int Count = 0; Count < 3; Count++)
+            {
+                Tulos = true;
+                Console.Write("Anna luku: ");
+                Tulos = int.TryParse(Console.ReadLine(), out Jono[Count]);
+                if (!Tulos)
+                {
+                    Console.WriteLine("Et antanut lukua! Yritä uudestaan!");
+                    Count = Count - 1;
+                }
+            }
+
+            Array.Sort(Jono);
+
+            Console.WriteLine("Luvut ovat " + Jono[0] + ", " + Jono[1] + ", " + Jono[2] + " nousevassa järjestyksessä.");
+            Console.ReadKey();
+            
         }
     }
 }
